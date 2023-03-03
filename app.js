761b93b0be8fd2bf7b77b61da7e3414d00ccfe2b
AhuraMagda <104498979+AhuraMagda@users.noreply.github.com>
2023-03-03 01:01:58 +0100

    ikonki wszystkie

--- app.js
+++ app.js
@@ -65,21 +65,38 @@ imagesObserver.observe(imagesBox)
 
 // chowanie i pokazywanie opisu obrazka na przykładzie laptopa
 
-const laptopCap = document.getElementById('laptop-title');
-const laptopText = document.getElementById('laptop-text');
-const laptopImg = document.getElementById('laptop-img');
-const arrows = document.querySelectorAll('.triangle')
+// 1 laptop
+const laptopText = document.querySelector('#laptop p')
+const laptopImg = document.querySelector('#laptop img')
+const laptopArrow = document.querySelector('#serwis div')
 
-const toggleVisibility = () => {
-    if (laptopText.style.display === 'block') {
-        laptopText.style.display = 'none'
-        laptopImg.style.opacity = '1';
+// 2 service
+const serviceText = document.querySelector('#serwis p')
+const serviceImg = document.querySelector('#serwis img')
+const serviceArrow = document.querySelector('#serwis div')
+
+// 3 CPU
+// 4 printer
+// 5 windows
+// 6 mouse
+
+// const toggleVisibility = () => {
+//     if (laptopText.style.display === 'block') {
+//         laptopText.style.display = 'none'
+//         laptopImg.style.opacity = '1';
+//     } else {
+//         laptopText.style.display = 'block'
+//         laptopImg.style.opacity = '0.2';
+//     }
+// }
+
+const toggleVisibility = (text, img) => {
+    if (text.style.display === 'block') {
+        text.style.display = 'none'
+        img.style.opacity = '1';
     } else {
-        laptopText.style.display = 'block'
-        laptopImg.style.opacity = '0.2';
+        text.style.display = 'block'
+        img.style.opacity = '0.2';
     }
 }
 
-arrows.forEach( (arrow, i) => {
-    arrow.addEventListener('click', toggleVisibility);
-})
